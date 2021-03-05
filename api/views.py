260c8c1df464979b388af61fdaa527b061862b26
Yasiruofficial <yasiruswaris@gmail.com>
2021-03-05 19:59:31 +0530

    dashboard and api added

--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+
 
 # Create your views here.
+
+def testapi(request):
+    data = {
+        'institute_name': 'Informatics Institute of Technology (IIT)',
+        'project': 'sdgp',
+        'project_title': 'water_quality_meter',
+        'year': 2
+        }
+    return JsonResponse(data)
