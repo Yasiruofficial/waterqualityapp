6ef1e1a356d5ea76ef81289a9ba7b6b8cbaed721
yasiru <yasiruswaris@gmail.com>
2021-04-19 03:18:53 +0000

    Update app to demonstrate build triggers

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from waterqualityapp.wsgi import application
-# App Engine by default looks for a main.py file at the root of the app
-# directory with a WSGI-compatible object called app.
-# This file imports the WSGI-compatible object of the Django app,
-# application from mysite/wsgi.py and renames it app so it is
-# discoverable by App Engine without additional configuration.
-# Alternatively, you can add a custom entrypoint field in your app.yaml:
-# entrypoint: gunicorn -b :$PORT mysite.wsgi
-app = application
+#!/usr/bin/env python
+
+import waterqualityapp
+
+class MainHandler(waterqualityapp.RequestHandler):
+    def get(self):
+        self.response.write('I update automatically!')
+
+app = waterqualityapp.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
