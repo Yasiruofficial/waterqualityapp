96707949417f8155f876611e7b4af711b8b6921e
Yasiru Swaris <52125232+Yasiruofficial@users.noreply.github.com>
2021-04-19 09:03:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python
+from waterqualityapp.wsgi import application
 
-import waterqualityapp
-
-class MainHandler(waterqualityapp.RequestHandler):
-    def get(self):
-        self.response.write('I update automatically!')
-
-app = waterqualityapp.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+app = application
