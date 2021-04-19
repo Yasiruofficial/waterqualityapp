fb0d0641a802fb297ae4cb53a7ddb784b58eee42
yasiru <yasiruswaris@gmail.com>
2021-04-19 02:56:03 +0000

    Add cloudbuild.yaml file

--- api/views.py
+++ api/views.py
@@ -6,22 +6,24 @@ from api.models import Data, Device
 def post_data(request):
     api_key = "95d465b0-0e99-4619-9553-186c4b5d294e"
 
-    if request.GET["api_key"] == api_key:
+    # if request.GET["api_key"] == api_key:
 
-        if 'ph' in request.GET and \
-                'us' in request.GET and \
-                'hd' in request.GET and \
-                'ts' in request.GET and \
-                'device' in request.GET:
+    #     if 'ph' in request.GET and \
+    #             'us' in request.GET and \
+    #             'hd' in request.GET and \
+    #             'ts' in request.GET and \
+    #             'device' in request.GET:
 
-            data = Data()
-            data.ph = request.GET["ph"]
-            data.us = request.GET["us"]
-            data.hd = request.GET["hd"]
-            data.ts = request.GET["ts"]
-            data.device_id = request.GET["device"]
-            data.save()
+    #         data = Data()
+    #         data.ph = request.GET["ph"]
+    #         data.us = request.GET["us"]
+    #         data.hd = request.GET["hd"]
+    #         data.ts = request.GET["ts"]
+    #         data.device_id = request.GET["device"]
+    #         data.save()
 
-        return HttpResponse(" Inserted =$= ")
-    else:
-        return HttpResponse(" Error on Saving =$= ")
+    #         return HttpResponse(" Inserted =$= ")
+    #     else:
+    #         return HttpResponse(" Error on Saving =$= ")
+    # else:
+    return HttpResponse(" Worng API_KEY =$= ")
--- waterqualityapp/settings.py
+++ waterqualityapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'qk^=q1i2__wvij)w51masz!1#cnfaslon^*y+$uvou#h668ypg'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','waterqualityapp.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -76,15 +76,7 @@ WSGI_APPLICATION = 'waterqualityapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sdgp',
-        'USER': 'postgres',
-        'PASSWORD': 'sdgp123',
-        'HOST': 'localhost',
-    }
-}
+DATABASES = { }
 
 
 # Password validation
