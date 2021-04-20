8884491a2cfe669410014ad53f0be2bd8aa41854
Yasiruofficial <yasiruswaris@gmail.com>
2021-04-20 08:34:34 +0530

    Update

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-from waterqualityapp.wsgi import application
+#!/usr/bin/env python
 
-app = application
+import webapp2
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('I update automatically!')
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
--- waterqualityapp/settings.py
+++ waterqualityapp/settings.py
@@ -65,13 +65,7 @@ WSGI_APPLICATION = 'waterqualityapp.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'waterqualitymeter',
-        'USER': 'postgres',
-        'PASSWORD': 'YAsiru29324253',
-        'HOST': '35.239.254.139',
-    }
+
 }
 
 
