e4e4bb52f0c9eb1da4242bad9fa38c06148ad1f9
Yasiruofficial <yasiruswaris@gmail.com>
2021-04-21 19:56:19 +0530

    Update TIME - v2.7

--- api/models.py
+++ api/models.py
@@ -20,7 +20,7 @@ class Data(models.Model):
     tm = models.CharField(max_length=20, null=True)
     hu = models.CharField(max_length=20, null=True)
     wt = models.CharField(max_length=20, null=True)
-    time = models.DateTimeField(default=datetime.now,)
+    time = models.DateTimeField(default=datetime.now)
     device = models.ForeignKey(Device, on_delete=models.CASCADE)
 
 
